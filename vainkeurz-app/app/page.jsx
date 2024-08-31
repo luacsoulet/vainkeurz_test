2237af39c701d5b61bbff6fd1a01fdf332bd9b7c
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-08-31 05:50:08 +0200

    Update page.jsx

--- vainkeurz-app/app/page.jsx
+++ vainkeurz-app/app/page.jsx
@@ -1,9 +1,27 @@
+'use client';
 
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello</div>
+    <main className="flex min-h-screen w-[1240px] flex-col items-center justify-center p-10 bg-gradient-to-br from-[#62198d] via-[#300d68] to-[#280463] text-white mt-5 rounded-lg">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        className="text-6xl font-bold mb-8"
+      >
+        Page d'accueil
+      </motion.div>
+      
+      <motion.p
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: 'easeInOut', delay: 0.5 }}
+        className="text-lg text-center max-w-md"
+      >
+        Bienvenue sur la page d'accueil ! Utilisez le menu à gauche pour naviguer vers les autres pages.
+      </motion.p>
     </main>
   );
 }
